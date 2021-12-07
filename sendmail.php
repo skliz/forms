fb1b4ddce117f4729ad7e5c2292ed31e3354f456
skliz <91405838+skliz@users.noreply.github.com>
2021-12-07 22:49:45 +0300

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -9,6 +9,16 @@ $mail = new PHPMailer(true);
 $mail->CharSet = 'UTF-8';
 $mail->setLanguage('ru', 'phpmailer/language/');
 $mail->IsHTML(true);
+    //Server settings
+
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'user@example.com';                     //SMTP username
+    $mail->Password   = 'secret';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;     
 
 // От кого письмо
 $mail->setForm('info@fls.guru', 'Фрилансер по жизни!');
